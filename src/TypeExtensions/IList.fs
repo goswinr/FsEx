b86dc2605ba67ee605fbae3de99cb002528e1318
Goswin <gwinsky@yahoo.com>
2020-04-30 15:07:18 +0200

    Update IList.fs

--- src/TypeExtensions/IList.fs
+++ src/TypeExtensions/IList.fs
@@ -9,54 +9,75 @@ module TypeExtensionsIList =
 
     //[<Extension>] //Error 3246
     type Collections.Generic.IList<'T>  with 
+        
+        /// Gets the index of the last item in the IList.
+        /// equal to this.Length - 1   
         [<Extension>]
         member inline this.LastIndex = 
             if this.Count = 0 then failwithf "this.LastIndex: Can not get LastIndex of empty IList"
             this.Count - 1
 
+        /// Gets the last item in the IList.
+        /// equal to this.[this.Length - 1]
         [<Extension>]
         member inline this.Last = 
             if this.Count = 0 then failwithf "this.Last: Can not get Last item of empty IList"
             this.[this.Count - 1]
 
+        /// Gets the second last item in the IList.
+        /// equal to this.[this.Length - 2]
         [<Extension>]
         member inline this.SecondLast = 
             if this.Count < 2 then failwithf "this.SecondLast: Can not get SecondLast item of %s"  (NiceString.toNiceStringFull this)
             this.[this.Count - 2]
 
+        /// Gets the third last item in the IList.
+        /// equal to this.[this.Length - 2]
         [<Extension>]
         member inline this.ThirdLast = 
             if this.Count < 3 then failwithf "this.ThirdLast: Can not get ThirdLast item of %s"  (NiceString.toNiceStringFull this)
             this.[this.Count - 3]
 
 
+        /// Gets the first item in the IList.
+        /// equal to this.[0]
         [<Extension>]
         member inline this.First = 
             if this.Count = 0 then failwithf "this.First: Can not get First item of empty IList"
             this.[0]
 
+        /// Gets the second item in the IList.
+        /// equal to this.[1]
         [<Extension>]
         member inline this.Second = 
             if this.Count < 2 then failwithf "this.Second: Can not get Second item of %s"  (NiceString.toNiceStringFull this)
             this.[1]
 
+        /// Gets the third item in the IList.
+        /// equal to this.[2]
         [<Extension>]
         member inline this.Third = 
             if this.Count < 3 then failwithf "this.Third: Can not get Third item of %s"  (NiceString.toNiceStringFull this)
             this.[2]
 
             
+        /// Gets an item in the IList by index.
+        /// Allows for negtive index too ( -1 is last item,  like Python)
+        /// (from the release of F# 5 on a negative index can also be done with '^' prefix. E.g. ^0 for the last item)
         [<Extension>] 
-        /// Allows for negtive index too (like Python)
         member this.GetItem index = if index<0 then this.[this.Count+index]   else this.[index]
     
+        /// Sets an item in the IList by index.
+        /// Allows for negtive index too ( -1 is last item, like Python)
+        /// (from the release of F# 5 on a negative index can also be done with '^' prefix. E.g. ^0 for the last item)
         [<Extension>] 
-        /// Allows for negtive index too (like Python)
         member this.SetItem index value = if index<0 then this.[this.Count+index]<-value   else this.[index]<-value  // only on IList
 
-
-        /// Allows for negative indices too.
-        /// The resulting List includes the item at slice ending index.
+        /// Gets a subrange of the IList
+        /// Allows for negative indices too. ( -1 is last item, like Python)
+        /// The resulting array includes the end index.
+        /// The built in slicing notaion (e.g. a.[1..3]) for arrays does not allow for negative indices.
+        /// (from the release of F# 5 on a negative index can also be done with '^' prefix. E.g. ^0 for the last item)
         [<Extension>]
         member this.Slice(startIdx:int, endIdx:int) : 'T ResizeArray = // to use slicing notation e.g. : xs.[ 1 .. -1] // don't overload .GetSlive .[ x ... y] directly, this would be a casting horror for Lists and arrays wher neg indices  Slices dont work
             let count = this.Count
