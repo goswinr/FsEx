da98c68c47e719ca21fdb97f538fa5805c0b1d2a
Goswin <gwinsky@yahoo.com>
2020-07-24 12:29:27 +0200

    Int.AsString

--- Src/TypeExtensions/BclStructs.fs
+++ Src/TypeExtensions/BclStructs.fs
@@ -6,45 +6,63 @@ open System.Runtime.CompilerServices
 
 
 [<AutoOpen>]
-module TypeExtensionsStructs =      
+module TypeExtensionsBclStructs =      
 
-    //[<Extension>] //Error 3246
+ 
     type Int32 with  
+        
+        /// Calls float(x)
         [<Extension>] member inline x.ToFloat = float(x)
+        
+        /// Calls byte(x)
         [<Extension>] member inline x.ToByte = byte(x)
+        
+        /// Same as i.ToString() but without the need for the brackets
+        // (for nicer inlining)
+        [<Extension>] member inline x.AsString = x.ToString()
 
-    //[<Extension>] //Error 3246
+ 
     type Byte with  
+        /// Calls float(x)
         [<Extension>] member inline x.ToFloat = float(x)
+
+        /// Calls byte(x)
         [<Extension>] member inline x.ToInt = int(x)
     
     
-    //[<Extension>] //Error 3246
+ 
     type Double with  
-        /// converts int to float including rounding: 
+        /// Converts int to float INCLUDING ROUNDING .
         /// int(round(x))
         [<Extension>] member inline x.ToInt = int(round(x))
 
-        /// with automatic formating of display precision depending on float size
+        /// With automatic formating of display reduced precision depending on float size
         [<Extension>] member x.ToNiceString = NiceString.floatToString x        
         
-    //[<Extension>] //Error 3246
+        /// Similar to f.ToString() but
+        /// with automatic formating to never use scientific notation
+        /// will have maximum 15 decimal places
+        /// f.ToString( "0.###############") 
+        [<Extension>] member x.AsString = x.ToString( "0.###############") 
+ 
     type Single with  
-        /// with automatic formating of display precision depending on float size
+
+        /// with automatic formating of display reduced precision depending on single size
         [<Extension>] member x.ToNiceString = NiceString.singleToString x
 
     
-    //[<Extension>] //Error 3246
     type DateTime with
         
-        /// Current date as yyyy-MM-dd
+        /// Current local date as yyyy-MM-dd
         [<Extension>]static member todayStr =    DateTime.Now.ToString("yyyy-MM-dd")
         
         /// Current local date and time as yyyy-MM-dd_HH-mm
         [<Extension>]static member nowStr =      DateTime.Now.ToString("yyyy-MM-dd_HH-mm")
         
         /// Current UTC date and time as yyyy-MM-dd_HH-mm_UTC
+        /// (with _UTC suffix)
         [<Extension>]static member nowStrUtc =   DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm_UTC")
         
-        /// Current UTC date and time as yyyy-MM-dd_HH-mm-fff. inludes 3 digits of miliseconds
+        /// Current UTC date and time as yyyy-MM-dd_HH-mm-fff
+        /// (inludes 3 digits of miliseconds)
         [<Extension>]static member nowStrLong =   DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss-fff")
