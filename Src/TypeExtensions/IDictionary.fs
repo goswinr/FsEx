eb871d4c3092d3cadf14ffed46f57e8bcea5c4a8
Goswin <gwinsky@yahoo.com>
2021-02-03 15:10:47 +0100

    Dict.tryGet

--- Src/TypeExtensions/IDictionary.fs
+++ Src/TypeExtensions/IDictionary.fs
@@ -54,13 +54,19 @@ module TypeExtensionsIDictionary =
 module Dict = 
     
     /// Get value at key from IDictionary, with nicer Error messages
-    let get (k:'K) (d:IDictionary<'K,'V>) : 'V = 
-        let ok, v = d.TryGetValue(k)
+    let get (key:'K) (d:IDictionary<'K,'V>) : 'V = 
+        let ok, v = d.TryGetValue(key)
         if ok then  v
-        else KeyNotFoundException.Raise "Dict.get faild to find key %A in %A of %d items" k d d.Count
+        else KeyNotFoundException.Raise "Dict.get faild to find key %A in %A of %d items" key d d.Count
     
     /// Set value at key from IDictionary
     // just d.[k]<-v
     let set (value:'V) (key:'K) (dict:IDictionary<'K,'V>) =  
         dict.[key]<-value
+
+    
+    let tryGet (k:'K) (d:IDictionary<'K,'V>) : 'V option= 
+        let ok, v = d.TryGetValue(k)
+        if ok then  Some v
+        else None
         
