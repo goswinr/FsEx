4c05bd9e717d4bffec47bcb1626fac8d034c61bd
Goswin <gwinsky@yahoo.com>
2021-03-04 13:30:23 +0100

    Update add null checks.fsx

--- Tools/add null checks.fsx
+++ Tools/add null checks.fsx
@@ -2,28 +2,19 @@
 //#r @"D:\OneDrive\10_Dev\01_Nuget\fparsec.1.1.1\lib\netstandard2.0\FParsecCS.dll"
 //#r @"D:\OneDrive\10_Dev\01_Nuget\fparsec.1.1.1\lib\netstandard2.0\FParsec.dll"
 
-
 open System
 open FsEx
-
 
 let file = @"D:\Git\FsEx\Src\StringModule.fs"
 
 let ilns = IO.File.ReadAllLines(file) 
 
-
-
 type Arg =  
     |Str of string
     |Cha of string
+    |ChAr of string
     |Int of string 
-    //|Oth of string
-    
-    member this.Name =  
-        match this with 
-        |Str n
-        |Cha n
-        |Int n -> n
+    |Oth of string 
     
 
 let getRaiseFail (a:Arg) (n:String) (decl:string)  (args:Rarr<Arg>) = 
@@ -42,16 +33,23 @@ let getRaiseFail (a:Arg) (n:String) (decl:string)  (args:Rarr<Arg>) =
                 match o with 
                 |Str n -> yield n;  yield ":%s) "
                 |Cha n -> yield n;  yield ":%c) "
+                |ChAr n -> yield n; yield ":%A) "
                 |Int n -> yield n;  yield ":%d) "
+                |Oth n ->  
+                    yield n;  
+                    if   n="culture" then   yield ":%A) "
+                    elif n="options" then   yield ":%A) "
+                    else                    yield ":%d) "
         yield "\"" 
         for o in args do  
             if o<>a then 
                 match o with 
-                |Str n -> yield " "; yield n
-                |Cha n -> yield " "; yield n
-                |Int n -> yield " "; yield n
-        }
-        
+                |Str  n -> yield " (exnf "; yield n;  yield ")"
+                |Cha  n -> yield " "; yield n
+                |ChAr n -> yield " "; yield n
+                |Int  n -> yield " "; yield n
+                |Oth  n -> yield " "; yield n
+        } 
         
         
 let getChecks(s:string) =
@@ -69,10 +67,13 @@ let getChecks(s:string) =
     
     let args =  
         rarr{ for a in args do  
-                if   a.Contains ":string" then  Str (String.beforeChar ':'      (a.Trim( [| '('; ')' |]) ) ) 
-                elif a.Contains ":char" then    Cha (String.beforeChar ':'      (a.Trim( [| '('; ')' |]) ) )  
-                else                            Int (String.beforeCharMaybe ':' (a.Trim( [| '('; ')' |]) ) ) 
-            }
+                if   a.Contains ":string[]" then  Oth (String.beforeChar ':'      (a.Trim( [| '('; ')' |]) ) ) 
+                elif a.Contains ":string"   then  Str (String.beforeChar ':'      (a.Trim( [| '('; ')' |]) ) ) 
+                elif a.Contains ":char[]"   then  ChAr(String.beforeChar ':'      (a.Trim( [| '('; ')' |]) ) )  
+                elif a.Contains ":char"     then  Cha (String.beforeChar ':'      (a.Trim( [| '('; ')' |]) ) )  
+                elif a.Contains ":int"      then  Int (String.beforeCharMaybe ':' (a.Trim( [| '('; ')' |]) ) ) 
+                else                              Oth (String.beforeCharMaybe ':' (a.Trim( [| '('; ')' |]) ) ) 
+            }                               
     
     
     rarr{  
@@ -97,7 +98,8 @@ rarr{
             yield! getChecks ln
         else 
             () 
-            //yield ln
+            yield ln
     }
             
-|> printFull
+//|> printFull
+|> fun xs -> IO.File.WriteAllLines(file, xs) 
