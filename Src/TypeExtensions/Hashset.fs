c655da0d395060a98312a88cca928437a2170a24
Goswin <gwinsky@yahoo.com>
2021-08-09 16:44:24 +0300

    own file for Hashset

---
+++ Src/TypeExtensions/Hashset.fs
@@ -0,0 +1,36 @@
+﻿namespace FsEx
+
+open System
+open System.Collections.Generic
+
+
+/// A type abbreviation for System.Collections.Generic.HashSet<'T> 
+/// so that it is available in FsEx namespace
+/// ( with a lowercase 's')
+type Hashset<'T> = HashSet<'T>
+
+/// AutoOpen Extensions for HashSet 
+/// provides DoesNotContain method
+[<AutoOpen>]
+module AutoOpenExtensionsHashset =       
+    open System.Runtime.CompilerServices
+
+    // This type extension should be alway availabe that s why it is in this Auitoopen module 
+    type HashSet<'T> with            
+        
+        /// Returns: not (set.Contains(key))
+        [<Extension>]
+        member inline h.DoesNotContain(key:'T) = 
+            not (h.Contains(key))
+        
+        /// Like this.Add(key), but returning unit.
+        /// h.Add(key)|> ignore
+        [<Extension>]
+        member inline h.add(key:'T) = 
+            h.Add(key)|> ignore
+        
+        /// Like this.Remove(key), but returning unit.
+        /// h.Remove(key)|> ignore
+        [<Extension>]
+        member inline h.remove(key:'T) = 
+            h.Remove(key)|> ignore
--- Src/Types/Dict.fs
+++ Src/Types/Dict.fs
@@ -4,39 +4,6 @@ open System
 open System.Collections.Generic
 
 
-/// A type abbreviation for System.Collections.Generic.HashSet<'T> 
-/// so that it is available in FsEx namespace
-/// ( with a lowercase 's')
-type Hashset<'T> = HashSet<'T>
-
-/// AutoOpen Extensions for HashSet 
-/// provides DoesNotContain method
-[<AutoOpen>]
-module AutoOpenExtensionsHashset =       
-    open System.Runtime.CompilerServices
-
-    // This type extension should be alway availabe that s why it is in this Auitoopen module 
-    type HashSet<'T> with            
-        
-        /// Returns: not (set.Contains(key))
-        [<Extension>]
-        member inline h.DoesNotContain(key:'T) = 
-            not (h.Contains(key))
-        
-        /// Like this.Add(key), but returning unit.
-        /// h.Add(key)|> ignore
-        [<Extension>]
-        member inline h.add(key:'T) = 
-            h.Add(key)|> ignore
-        
-        /// Like this.Remove(key), but returning unit.
-        /// h.Remove(key)|> ignore
-        [<Extension>]
-        member inline h.remove(key:'T) = 
-            h.Remove(key)|> ignore
-
-
-
 /// A thin wraper over System.Collections.Generic.Dictionary<'K,'V>) with nicer Error messages on accessing missing keys.
 /// There is a hidden member called "Dictionary" to access the underlaying Collections.Generic.Dictionary<'K,'V> directly.
 /// In F# use #nowarn "44" to disable the obsolete warning for this hidden member.
