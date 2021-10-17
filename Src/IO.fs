399c344eaf2792da582d22cc9ebebc84854ba84f
Goswin <gwinsky@yahoo.com>
2021-10-17 20:27:44 +0200

    IO.getAllFilesInParents

--- Src/IO.fs
+++ Src/IO.fs
@@ -7,50 +7,107 @@ open System.Threading
 
 
 //[<AutoOpen>]
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>] //need this so doesn't hide IO namespace in C# assemblies 
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>] //need this so doesn't hide IO namespace in C# assemblies
 module IO = 
+   
+   /// Returns the path to the current Desktop (exluding a trailing slash)
+    /// Containg forward slashes only
+    /// Environment.GetFolderPath(Environment.SpecialFolder.Desktop).Replace('\\','/') 
+    let desktop = 
+        Environment.GetFolderPath(Environment.SpecialFolder.Desktop).Replace('\\','/') 
+
+    
+    /// Returns the path to the current Desktop(including a trailing slash '/').
+    /// Containg forward slashes only
+    /// Environment.GetFolderPath(Environment.SpecialFolder.Desktop).Replace('\\','/')  + "/"
+    let desktopS = 
+        Environment.GetFolderPath(Environment.SpecialFolder.Desktop).Replace('\\','/')  + "/"
+
 
     /// Raises an FileNotFoundException if the file path does not exist
     let checkIfFileExists s = 
         if not (IO.File.Exists s) then  raise (FileNotFoundException("File missing or path worng: '" + s + "'"))
-     
+
      /// Raises an DirectoryNotFoundException if the directory path does not exist
     let checkIfDirectoryExists s = 
-        if not (IO.Directory.Exists s) then  raise (DirectoryNotFoundException("Directory missing or path worng: '" + s + "'"))     
+        if not (IO.Directory.Exists s) then  raise (DirectoryNotFoundException("Directory missing or path worng: '" + s + "'"))
+
+    /// Returns all files in folder and subfolders.
+    /// Returns first the files from this folder then from first child folder with all its grandchildren
+    /// then next child folder with all its granddchildren...
+    /// May fail with IOExceptions.
+    let getAllFiles (directory:string) = 
+        if not (Directory.Exists(directory)) then
+            raise (DirectoryNotFoundException("IO.getAllFiles: Directory '" + directory + "' not found"))
+        let rec get (dir:string) = 
+            seq {   yield! Directory.EnumerateFiles(dir)
+                    for d in Directory.EnumerateDirectories(dir) do yield! get d }
+        get directory
+
+    /// Returns all files in folder and subfolders that fit pattern (e.g. '*.pdf').
+    /// Returns first the files from this folder then from first child folder with all its grandchildren
+    /// then next child folder with all its granddchildren...
+    /// Wildcard specifiers:
+    /// * (asterisk)    Zero or more characters in that position.
+    /// ? (question mark)    Zero or one character in that position.
+    /// May fail with IOExceptions.
+    let getAllFilesByPattern (pattern:string) (directory:string) = 
+        if not (Directory.Exists(directory)) then
+            raise (DirectoryNotFoundException("IO.getAllFilesByPattern: Directory '" + directory + "' not found for file pattern : '"+pattern+"'"))
+        let rec get (dir:string) = 
+            seq {   yield! Directory.EnumerateFiles(dir, pattern)
+                    for d in Directory.EnumerateDirectories(dir) do yield! get d }
+        get directory  
     
     /// Returns all files in folder and subfolders
-    /// Ignores all errors and moves on to next folder
-    let rec getAllFiles (dir:string) = 
-        seq { if Directory.Exists dir then 
-                let files = try Directory.GetFiles(dir) with _ -> [||]
-                Array.sortInPlace files
-                yield! files
-                let dirs = try Directory.GetDirectories(dir) with _ -> [||]
-                Array.sortInPlace dirs
-                for subdir in dirs do 
-                    yield! getAllFiles subdir }
-
-    /// Returns all files in folder and subfolders that fit pattern (e.g. "*.pdf" ) 
-    /// may fail on IOExceptions
-    let rec getAllFilesByPattern (dir:string) (pattern:string) =
-        seq {   yield! Directory.EnumerateFiles(dir, pattern)
-                for d in Directory.EnumerateDirectories(dir) do
-                    yield! getAllFilesByPattern d pattern }
-
-    /// Returns the path to the current Desktop
-    /// Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
-    let desktop = 
-        Environment.GetFolderPath(Environment.SpecialFolder.Desktop)   
+    /// Ignores all errors except if the initial directory does not exist and moves on to next folder.
+    /// Returns first the files from this folder (sorted) then from first child folder with all its grandchildren
+    let getAllFilesSave (directory:string) = 
+        if not (Directory.Exists(directory)) then
+            raise (DirectoryNotFoundException("IO.getAllFilesSave: Directory '" + directory + "' not found "))
+        let rec getAll (dir:string) = 
+            seq { if Directory.Exists dir then
+                    let files = try Directory.GetFiles(dir) with _ -> [||]
+                    Array.Sort(files)// a before Z
+                    //Array.sortInPlace files // Z before a
+                    yield! files
+                    let dirs = try Directory.GetDirectories(dir) with _ -> [||]
+                    Array.Sort(dirs)// a before Z
+                    //Array.sortInPlace dirs // Z before a
+                    for subdir in dirs do
+                        yield! getAll subdir }
+        getAll(directory)
     
+    /// Returns all files in this folder and parent folders that fit pattern (e.g. '*.pdf').
+    /// Wildcard specifiers:
+    /// * (asterisk)        Zero or more characters in that position.
+    /// ? (question mark)   Zero or one character in that position.
+    /// May fail with IOExceptions
+    let getAllFilesInParentsByPattern (pattern:string) (directory:string) = 
+        if not (Directory.Exists(directory)) then
+            raise (DirectoryNotFoundException("IO.findFileInParentsAndSiblingsByPattern: Directory '" + directory + "' not found for file pattern : '"+pattern+"'"))        
+        let rec find (dir:string) =  
+            seq{  
+                yield! Directory.EnumerateFiles(dir, pattern)
+                let par = Directory.GetParent(dir)
+                if isNull par then // reached root 
+                    () 
+                else  
+                    yield! find par.FullName      }
+        find directory 
+    
+
 
     /// Determines a text file's encoding by analyzing its byte order mark (BOM).
     /// Returns None  when detection of the text file's endianness fails.( might be ASCII or UTF-8 without BOM)
     /// (Encoding.Unicode = UTF-16LE)
-    let getEncoding(filename:string)=
-        // https://stackoverflow.com/a/19283954/969070         
+    [<Obsolete("The results of this cannot be relied upon. see https://stackoverflow.com/a/12853721/969070")>]
+    let getEncoding(filename:string)= 
+        // https://stackoverflow.com/a/19283954/969070
+        // better: https://stackoverflow.com/a/12853721/969070
         let bom = Array.zeroCreate 4
-        use  file = new FileStream(filename, FileMode.Open, FileAccess.Read) // Read the BOM       
-        if file.Read(bom, 0, 4) <> 4 then None else         
+        use  file = new FileStream(filename, FileMode.Open, FileAccess.Read) // Read the BOM
+        if file.Read(bom, 0, 4) <> 4 then None else
             // Analyze the BOM
             if   bom.[0] = 0x2buy && bom.[1] = 0x2fuy && bom.[2] = 0x76uy then  Some  Encoding.UTF7
             elif bom.[0] = 0xefuy && bom.[1] = 0xbbuy && bom.[2] = 0xbfuy then  Some  Encoding.UTF8
@@ -58,79 +115,154 @@ module IO =
             elif bom.[0] = 0xffuy && bom.[1] = 0xfeuy then  Some  Encoding.Unicode; //UTF-16LE
             elif bom.[0] = 0xfeuy && bom.[1] = 0xffuy then  Some  Encoding.BigEndianUnicode; //UTF-16BE
             elif bom.[0] = 0uy    && bom.[1] = 0uy    && bom.[2] = 0xfeuy && bom.[3] = 0xffuy then Some  ( UTF32Encoding(true, true) :> Encoding)   //UTF-32BE
-            else 
+            else
                 // We actually have no idea what the encoding is if we reach this point, might be ASCII or UTF-8 without BOM
                 None
-
     
-    /// Reads and Writes with Lock, 
+    /// For non blocking File IO
+    module File  = 
+        
+        /// Reads all bytes from a file in a non blocking manner
+        /// This can avoid exceptions such as 'The process cannot access the file'.
+        /// It is using FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+        let readAllBytesNonBlocking (path:string) = 
+            // using a stream allows for non blocking file access
+            use stream  = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+            use mstream = new MemoryStream(int stream.Length)
+            stream.CopyTo(mstream)
+            let arr = mstream.ToArray()
+            stream.Dispose()
+            mstream.Dispose()
+            arr
+
+        /// Returns a memory stream, the file stream used is closed and disposed.
+        /// This can avoid exceptions such as 'The process cannot access the file'.
+        /// It is using FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+        let getNonBlockingStream (path:string) = 
+            // using a stream allows for non blocking file access
+            use stream  = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+            use mstream = new MemoryStream(int stream.Length)
+            stream.CopyTo(mstream)
+            stream.Dispose()
+            mstream
+
+
+
+    /// Reads and Writes with Lock,
     /// Optionally only once after a delay in which it might be called several times
-    /// using Text.Encoding.UTF8
-    type SaveReadWriter (path:string)= 
-        // same class also exist in  FsEx.Wpf , TODO keep in sync!
-       
+    /// Uses Text.Encoding.UTF8
+    /// Writes Exceptions to errorLogger because it is tricky to catch exceptions form an async thread
+    type SaveReadWriter (path:string, errorLogger:string->unit)= 
+        // same class also exist in FsEx , TODO keep in sync!
+
         let counter = ref 0L // for atomic writing back to file
-       
+
         let lockObj = new Object()
-        
-        /// calls IO.File.Exists(path)
-        member this.FileExists() = IO.File.Exists(path)
+
+        let maxCharsInString = 500
+
+        /// If the input string is longer than maxChars + 20 then
+        /// it returns the input string trimmed to maxChars, a count of skiped characters and the last 6 characters (all enclosed in double quotes ")
+        /// e.g. "abcde[..20 more Chars..]xyz"
+        /// Else, if the input string is less than maxChars + 20, it is still returned in full (enclosed in double quotes ").
+        /// also see String.truncatedFormated
+        let truncateString (stringToTrim:string) = 
+            if isNull stringToTrim then "-null string-" // add too, just in case this gets called externally
+            elif stringToTrim.Length <= maxCharsInString + 20 then sprintf "\"%s\""stringToTrim
+            else
+                let len   = stringToTrim.Length
+                let st    = stringToTrim.Substring(0, maxCharsInString)
+                let last20 = stringToTrim.Substring(len-21)
+                sprintf "\"%s[<< ... %d more chars ... >>]%s\"" st (len - maxCharsInString - 20) last20
+
+        /// Calls IO.File.Exists(path)
+        member this.FileExists = IO.File.Exists(path)
+
+        member this.FileDoesNotExists = not <| IO.File.Exists(path)
+
+        /// The full file path
+        member this.Path : string  = path
+
+        /// Creates file with text , only if it does not exist yet.
+        /// Writes Exceptions to errorLogger.
+        /// Returns true if file exists or was successfully created
+        member this.CreateFileIfMissing(text) :bool = 
+            if IO.File.Exists(path) then
+                true
+            else
+                try
+                    IO.File.WriteAllText(path, text,Text.Encoding.UTF8)
+                    true
+
+                with e ->
+                    errorLogger(sprintf "FsEx.Wpf.SaveReadWriter.CreateFileIfMissing for path '%s' :\r\n%A" path e)
+                    false
 
 
-        /// Save reading.
+        /// Thread Save reading.
         /// Ensures that no writing happens while reading.
-        member this.ReadAllText () : string =
+        /// Writes Exceptions to errorLogger
+        member this.ReadAllText () : option<string> = 
             // lock is using Monitor class : https://github.com/dotnet/fsharp/blob/6d91b3759affe3320e48f12becbbbca493574b22/src/fsharp/FSharp.Core/prim-types.fs#L4793
-            lock lockObj (fun () -> IO.File.ReadAllText(path, Text.Encoding.UTF8))            
-                
-    
-        /// Save reading.
+            lock lockObj (fun () ->
+                try Some <| IO.File.ReadAllText(path, Text.Encoding.UTF8)
+                with e ->
+                    errorLogger(sprintf "FsEx.Wpf.SaveReadWriter.ReadAllText from path '%s' :\r\n%A" path e)
+                    None  )
+
+
+        /// Thread Save reading.
         /// Ensures that no writing happens while reading.
-        member this.ReadAllLines () : string[] =
+        /// Writes Exceptions to errorLogger
+        member this.ReadAllLines () : option<string[]> = 
             // lock is using Monitor class : https://github.com/dotnet/fsharp/blob/6d91b3759affe3320e48f12becbbbca493574b22/src/fsharp/FSharp.Core/prim-types.fs#L4793
-            lock lockObj (fun () -> IO.File.ReadAllLines(path, Text.Encoding.UTF8))
-        
-        
+            lock lockObj (fun () ->
+                try Some <| IO.File.ReadAllLines(path, Text.Encoding.UTF8)
+                with e ->
+                    errorLogger(sprintf "FsEx.Wpf.SaveReadWriter.ReadAllText from '%s' :\r\n%A" path e)
+                    None  )
+
+
         /// File will be written async and with a Lock.
-        /// If it fails an Error is printed to the Error stream via eprintfn.
         /// Ensures that no reading happens while writing.
-        member this.WriteAsync (text) =        
+        /// Writes Exceptions to errorLogger
+        member this.WriteAsync (text) = 
             async{
                 lock lockObj (fun () -> // lock is using Monitor class : https://github.com/dotnet/fsharp/blob/6d91b3759affe3320e48f12becbbbca493574b22/src/fsharp/FSharp.Core/prim-types.fs#L4793
                     try  IO.File.WriteAllText(path,text, Text.Encoding.UTF8)
                     // try & with is needed because exceptions on threadpool cannot be caught otherwise !!
-                    with ex ->  eprintfn "FsEx.IO.SaveWriter.WriteAsync failed with: %A \r\n while writing to %s:\r\n%A" ex path text // use %A to trimm long text        
-                    )       
+                    with ex ->  errorLogger(sprintf "FsEx.Wpf.SaveWriter.WriteAsync failed with: %A \r\n while writing to %s:\r\n%A" ex path (truncateString text)) // use %A to trimm long text
+                    )
                 } |> Async.Start
-    
+
         /// File will be written async and with a Lock.
-        /// If it fails an Error is printed to the Error stream via eprintfn
         /// Ensures that no reading happens while writing.
-        member this.WriteAllLinesAsync (texts) =        
+        /// Writes Exceptions to errorLogger
+        member this.WriteAllLinesAsync (texts) = 
             async{
                 lock lockObj (fun () -> // lock is using Monitor class : https://github.com/dotnet/fsharp/blob/6d91b3759affe3320e48f12becbbbca493574b22/src/fsharp/FSharp.Core/prim-types.fs#L4793
                     try  IO.File.WriteAllLines(path,texts, Text.Encoding.UTF8)
                     // try & with is needed because exceptions on threadpool cannot be caught otherwise !!
-                    with ex ->  eprintfn "FsEx.IO.SaveWriter.WriteAllLinesAsync failed with: %A \r\n while writing to %s:\r\n%A" ex path texts // use %A to trimm long text        
-                    )       
+                    with ex ->  errorLogger(sprintf "FsEx.Wpf.SaveWriter.WriteAllLinesAsync failed with: %A \r\n while writing to %s:\r\n%A" ex path (Array.truncate 20 texts)) // use %A to trimm long text
+                    )
                 } |> Async.Start
-   
+
         /// GetString will be called in sync on calling thread, but file will be written async.
-        /// Only if after the delay the counter value is the same as before. 
+        /// Only if after the delay the counter value is the same as before.
         /// That means no more recent calls to this function have been made during the delay.
         /// If other calls to this function have been made then only the last call will be written as file.
-        /// If it fails an Error is printed to the Error stream via eprintfn.
         /// Also ensures that no reading happens while writing.
-        member this.WriteIfLast ( getText: unit->string, delayMillisSeconds:int) =
+        /// Writes Exceptions to errorLogger
+        member this.WriteIfLast ( getText: unit->string, delayMillisSeconds:int) = 
             async{
                 let k = Interlocked.Increment counter
                 do! Async.Sleep(delayMillisSeconds) // delay to see if this is the last of many events (otherwise there is a noticable lag in dragging window around, for example, when saving window position)
                 if !counter = k then //k > 2L &&   //do not save on startup && only save last event after a delay if there are many save events in a row ( eg from window size change)(ignore first two event from creating window)
-                    try 
-                        let text = getText() 
-                        this.WriteAsync (text) // this should never fail since exeptions are caught inside 
-                    with ex -> 
+                    try
+                        let text = getText()
+                        this.WriteAsync (text) // this should never fail since exeptions are caught inside
+                    with ex ->
                         // try & with is needed because exceptions on threadpool cannot be caught otherwise !!
-                        eprintfn "FsEx.IO.SaveWriter.WriteIfLast: getText() for path (%s) failed with: %A" path ex                 
-                } |> Async.StartImmediate            
+                        errorLogger(sprintf "FsEx.Wpf.SaveWriter.WriteIfLast: getText() for path '%s' failed with: %A" path ex )
+                } |> Async.StartImmediate
 
